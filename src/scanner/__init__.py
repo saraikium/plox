faeb3ea5ddf9a19fe4e0bbefa169db5a1e189d1c
saraikium <adnank122@gmail.com>
2022-04-10 09:21:58 +0500

    feat: add the ability to handle comment lexemes

--- src/scanner/__init__.py
+++ src/scanner/__init__.py
@@ -1,4 +1,5 @@
 from ..token import Token, TokenType
+from ..main import Lox
 
 
 class Scanner:
@@ -12,6 +13,17 @@ class Scanner:
         self.source = source
         self.tokens = []
 
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
+
+    def scan_tokens(self) -> list[Token]:
+        while self.is_at_end() is False:
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
     def advance(self) -> str:
 
         char = self.source[self.current]
@@ -22,9 +34,22 @@ class Scanner:
         text: str = self.source[self.start : self.current]
         self.tokens.append(Token(type, text, literal, self.line))
 
+    def match_next(self, expected: str) -> bool:
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != expected:
+            return False
+
+        self.current += 1
+        return True
+
+    def peek(self) -> str:
+        if self.is_at_end():
+            return "\0"
+        return self.source[self.current]
+
     def scan_token(self) -> None:
         c = self.advance()
-
         match c:
             case "(":
                 self.add_token(TokenType.LEFT_PAREN)
@@ -56,13 +81,45 @@ class Scanner:
             case "*":
                 self.add_token(TokenType.STAR)
 
-    def is_at_end(self) -> bool:
-        return self.current >= len(self.source)
+            case "!":
+                self.add_token(
+                    TokenType.BANG_EQUAL if self.match_next("=") else TokenType.BANG
+                )
+            case "=":
+                self.add_token(
+                    TokenType.EQUAL_EQUAL if self.match_next("=") else TokenType.EQUAL
+                )
 
-    def scan_tokens(self) -> list[Token]:
-        while self.is_at_end() is False:
-            self.start = self.current
-            self.scan_token()
+            case "<":
+                self.add_token(
+                    TokenType.LESS_EQUAL if self.match_next("=") else TokenType.LESS
+                )
 
-        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
-        return self.tokens
+            case ">":
+                self.add_token(
+                    TokenType.GREATER_EQUAL
+                    if self.match_next("=")
+                    else TokenType.GREATER
+                )
+
+            case "/":
+                if self.match_next("/"):
+                    while self.peek() != "\n" and self.is_at_end() == False:
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+
+            case " ":
+                pass
+
+            case "\r":
+                pass
+
+            case "\t":
+                pass
+
+            case "\n":
+                self.line += 1
+
+            case _:
+                Lox.error(self.line, "Unexpected character.")
